756f824adfb6368446a0f75b5c31b4e1b3097cf6
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-02 14:37:16 +0300

    Added the mounted function to console.log th state

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="wrapper">
     <h1>Welcome</h1>
-    <router-link to="/login"><button>Login</button></router-link>
-    <router-link to="home"><button>Home</button></router-link>
+    <!-- <router-link to="/login"><button>Login</button></router-link>
+    <router-link to="home"><button>Home</button></router-link> -->
     <router-link v-if="isLoggedIn" to="/login"
       ><button>Login</button></router-link
     >
@@ -10,16 +10,20 @@
 </template>
 
 <script>
-import store from '@/store';
-// import { mapGetters } from "vuex";
+import store from "@/store";
+import { mapGetters } from "vuex";
+
 export default {
   name: "WelcomeSection",
 
+  mounted() {
+    console.log(store.getters.isLoggedIn
+);
+  },
+
   //get state
-  computed: { 
-    isLoggedIn(){
-      return store.getters.isLoggedIn;
-    }
+  computed: {
+    ...mapGetters["isLoggedIn"],
   },
 };
 </script>
